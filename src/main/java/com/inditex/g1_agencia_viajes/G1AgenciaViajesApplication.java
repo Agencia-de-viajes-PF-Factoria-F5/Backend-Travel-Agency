f1f73dfdab9aed45284b3628a071ea5ad98b8047
joacundo2 <facundogo.garavagliaolivera@gmail.com>
2026-04-27 12:57:35 +0200

    feat:init Spring Boot project

---
+++ src/main/java/com/inditex/g1_agencia_viajes/G1AgenciaViajesApplication.java
@@ -0,0 +1,13 @@
+package com.inditex.g1_agencia_viajes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G1AgenciaViajesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G1AgenciaViajesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inditex/g1_agencia_viajes/G1AgenciaViajesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inditex.g1_agencia_viajes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G1AgenciaViajesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
