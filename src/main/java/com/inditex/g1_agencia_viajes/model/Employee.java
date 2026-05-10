8ff1f10a2c4a1caa9df3cd5a138fe9fc3817f42c
joacundo2 <facundogo.garavagliaolivera@gmail.com>
2026-05-10 12:10:44 +0200

    fix: employee propieties

--- src/main/java/com/inditex/g1_agencia_viajes/model/Employee.java
+++ src/main/java/com/inditex/g1_agencia_viajes/model/Employee.java
@@ -1,6 +1,5 @@
 package com.inditex.g1_agencia_viajes.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
