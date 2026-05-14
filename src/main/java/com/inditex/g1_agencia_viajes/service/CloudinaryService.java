7065736a1101574784b151ac09d7cd58f35bfa96
joacundo2 <facundogo.garavagliaolivera@gmail.com>
2026-05-14 11:06:28 +0200

    feat started configuration of cloudinary

---
+++ src/main/java/com/inditex/g1_agencia_viajes/service/CloudinaryService.java
@@ -0,0 +1,36 @@
+package com.inditex.g1_agencia_viajes.service;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+public class CloudinaryService {
+
+    private final Cloudinary cloudinary;
+
+    public CloudinaryService(Cloudinary cloudinary) {
+        this.cloudinary = cloudinary;
+    }
+
+    public String uploadImage(MultipartFile file) {
+        try {
+            Map<?, ?> uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.emptyMap());
+            return uploadResult.get("secure_url").toString();
+        } catch (IOException e) {
+            throw new RuntimeException("Error al subir la imagen a Cloudinary", e);
+        }
+    }
+
+    public void deleteImage(String publicId) {
+        try {
+            cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
+        } catch (IOException e) {
+            throw new RuntimeException("Error al eliminar la imagen de Cloudinary", e);
+        }
+    }
+}
